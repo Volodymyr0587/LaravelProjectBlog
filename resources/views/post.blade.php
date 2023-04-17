d8b570d411ae990fcd570f446595bb9e3beff4da
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-04-17 14:42:43 +0300

    Make a Route and Link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum illo neque fuga, nam cumque voluptate vitae,
+            assumenda soluta facilis in optio error aliquam quis ad deserunt adipisci blanditiis saepe veniam.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur autem voluptates voluptate, velit aperiam
+            tempore dolores consectetur, id corporis itaque, incidunt omnis dolor voluptas dicta eum sed. Ab, a impedit.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, repudiandae minima! Debitis provident
+            fuga neque. Repellat laborum amet placeat eum voluptate distinctio dolor aliquid natus in recusandae? Quod,
+            necessitatibus dignissimos?
+        </p>
+    </article>
+    <a href="/">Go Back</a>
+</body>
+
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum illo neque fuga, nam cumque voluptate vitae,
+            assumenda soluta facilis in optio error aliquam quis ad deserunt adipisci blanditiis saepe veniam.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur autem voluptates voluptate, velit aperiam
+            tempore dolores consectetur, id corporis itaque, incidunt omnis dolor voluptas dicta eum sed. Ab, a impedit.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, repudiandae minima! Debitis provident
+            fuga neque. Repellat laborum amet placeat eum voluptate distinctio dolor aliquid natus in recusandae? Quod,
+            necessitatibus dignissimos?
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum illo neque fuga, nam cumque voluptate vitae,
+            assumenda soluta facilis in optio error aliquam quis ad deserunt adipisci blanditiis saepe veniam.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe excepturi optio nihil voluptatibus quidem!
+            Facilis recusandae eos facere quod aperiam animi suscipit minima, ipsum libero. Quis dolor itaque soluta
+            quos.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nemo quos cumque, voluptatum officiis atque
+            doloribus, facilis, est pariatur fugiat enim necessitatibus soluta? Id libero esse dolores vel vero dolore.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum illo neque fuga, nam cumque voluptate vitae,
+            assumenda soluta facilis in optio error aliquam quis ad deserunt adipisci blanditiis saepe veniam.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, aliquid quos! Odit rem vitae animi
+            incidunt aspernatur architecto soluta facere tenetur cupiditate, quia consequuntur eius tempore hic nam
+            numquam perspiciatis.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis veritatis consequuntur voluptate fuga
+            cumque et consectetur exercitationem, voluptates nesciunt quisquam inventore quis nihil vero eaque sed
+            saepe, nemo beatae vitae!
+        </p>
+    </article>
+</body>
+
+</html>
