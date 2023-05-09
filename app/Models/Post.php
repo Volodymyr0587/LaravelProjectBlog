9e6b07d3c6af9816b7de188e95840586ab3e7d17
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-05-09 11:56:16 +0300

    EP 38 - Search (The Cleaner Way)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,9 +11,16 @@ class Post extends Model
     use HasFactory;
 
     protected $guarded = [];
-    // protected $fillable = ['title', 'excerpt', 'body'];
 
     protected $with = ['category', 'author'];
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, fn ($query, $search) =>
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%'));
+    }
 
     public function category()
     {
