9c19f953a5e7fe4a6b5f88d2d0f22c0ad29f6418
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-05-11 12:20:32 +0300

    EP 40 - Extract a Category Dropdown Blade Component

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
 
 
@@ -11,16 +10,14 @@ class PostController extends Controller
     public function index()
     {
 
-        return view('posts', [
-            'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
-            'categories' => Category::all(),
-            'currentCategory' => Category::firstWhere('slug', request('category'))
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(request(['search', 'category']))->get()
         ]);
     }
 
     public function show(Post $post)
     {
-        return view('post', [
+        return view('posts.show', [
             'post' => $post
         ]);
     }
