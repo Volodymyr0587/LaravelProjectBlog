27b056cae0e6629e56a43fafe6db9e0e5745dea0
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-05-16 13:40:46 +0300

    EP 47 - Failed Validation and Old Input Data

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -18,8 +18,13 @@
                         type="text"
                         name="name"
                         id="name"
+                        value="{{ old('name') }}"
                         required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -34,8 +39,13 @@
                         type="text"
                         name="username"
                         id="username"
+                        value="{{ old('username') }}"
                         required
                     >
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -50,6 +60,7 @@
                         type="email"
                         name="email"
                         id="email"
+                        value="{{ old('email') }}"
                         required
                     >
 
@@ -72,6 +83,10 @@
                         id="password"
                         required
                     >
+
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -81,6 +96,14 @@
                         Submit
                     </button>
                 </div>
+
+                {{-- @if ($errors->any())
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li class="text-red-500 text-xs">{{ $error }}</li>
+                    @endforeach
+                </ul>
+                @endif --}}
             </form>
         </main>
     </section>
